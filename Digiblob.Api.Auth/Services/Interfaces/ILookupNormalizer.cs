6aa3da02ccbee1030ecf98ce6e3b046ab287ff76
Marek Olsak <m_olsak@utb.cz>
2023-05-12 10:26:24 +0200

    Added lookup normalizer service .

---
+++ Digiblob.Api.Auth/Services/Interfaces/ILookupNormalizer.cs
@@ -0,0 +1,6 @@
+﻿namespace Digiblob.Api.Auth.Services.Interfaces;
+
+public interface ILookupNormalizer
+{
+    string Normalize(string userName);
+}
---
+++ Digiblob.Api.Auth/Services/UpperInvariantNormalizer.cs
@@ -0,0 +1,8 @@
+﻿using Digiblob.Api.Auth.Services.Interfaces;
+
+namespace Digiblob.Api.Auth.Services;
+
+public sealed class UpperInvariantNormalizer : ILookupNormalizer
+{
+    public string Normalize(string userName) => userName.Normalize().ToUpperInvariant();
+}
